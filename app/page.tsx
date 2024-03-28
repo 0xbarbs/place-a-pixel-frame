7969c63631fc4e3f0720f05b9633affc83976526
0xbarbs <0xbarbs@proton.me>
2024-03-28 03:25:22 +0000

    feat: added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import { getFrameMetadata } from 'frog/next'
 import type { Metadata } from 'next'
-import Image from 'next/image'
 
 import styles from './page.module.css'
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
-    `${process.env.NEXT_PUBLIC_URL || 'http://localhost:3000'}/api`,
-  )
+    `${process.env.NEXT_PUBLIC_URL || "http://localhost:3000"}/api`,
+  );
   return {
+    title: "Farcanvas",
+    description: 'The Farcaster driven pixel canvas',
     other: frameTags,
-  }
+  };
 }
 
 export default function Home() {
